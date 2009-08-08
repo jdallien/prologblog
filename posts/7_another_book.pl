a4614afe453b0f21454ff18185f5607d2c4e6cf0
Jeff Dallien <jeff@dallien.net>
2009-08-08 12:47:38 -0400

    Adding a new post

---
+++ posts/7_another_book.pl
@@ -0,0 +1,17 @@
+:- style_check(-atom).
+
+post(7, 'Another free PDF book: Prolog Techniques', 'Sat, 8 Aug 2009 12:41:00 -0400',
+      '
+<p>
+ <a href="http://bookboon.com/us/student/it/prolog-techniques-applications-of-prolog">Prolog Techniques</a> by Attila Csenki is a recently published Prolog ebook from the free ebook site <a href="http://bookboon.com">bookboon.com</a>.
+</p>
+<p> 
+ The books on the site are free because they are ad-sponsored. The ads come in the form of half-page full color ads every 3 to 4 pages. Advertisers in the Prolog Techniques book include Oracle, "Careers in British Intelligence" and other UK-based sites aimed mainly at university students. The
+ ads are a bit flashy for an otherwise calm and simple-looking book about Prolog, but after getting used to the idea I was able to read through the book easily, and I occasionally stopped to look at the ads, even though I wasn\'t in the target audience for them. I hope the bookboon.com site
+ will be able to survive based on this business model; having newly written material on Prolog is kind of exciting. Exciting on the scale of Prolog related things anyway. However, unless an international audience of programmers picks up on these books and thus shifts their 
+ collection of advertisers, it looks like their survival will depend on the uptake by students in the UK.
+</p>
+<p>
+I\'ll write more about my experiences with this book as I get through it.
+</p>
+     ').
